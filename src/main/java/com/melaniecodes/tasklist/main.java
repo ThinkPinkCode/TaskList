4726902d70a89218e4eb8518a295c3ddd705a17f
Melanie Myers <melanie.myers@365smartshop.com>
2017-05-09 11:33:09 -0400

    created list landing page

--- src/main/java/com/melaniecodes/tasklist/main.java
+++ src/main/java/com/melaniecodes/tasklist/main.java
@@ -3,6 +3,9 @@ package com.melaniecodes.tasklist;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
@@ -18,6 +21,12 @@ public class main {
 
         get("/chooseName", (req, res) -> {
             return new ModelAndView(null, "chooseName.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/list", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("listName", "List Name");
+            return new ModelAndView (model, "list.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
